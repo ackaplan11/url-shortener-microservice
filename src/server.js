4eae90632d93e2e784d6af7fa86198f354dd432b
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-03-23 09:33:04 -0700

    logging mongo_uri because connection string is the issue

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser')
 const app = express();
 
+console.log(process.env.MONGO_URI)
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected"))
   .catch((err) => console.log(err));
@@ -15,7 +17,6 @@ const schema = require('./schema.js')
 const URL = mongoose.model('URL', schema.urlSchema)
 
 // Basic Configuration
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/public', express.static(`${process.cwd()}/public`));
